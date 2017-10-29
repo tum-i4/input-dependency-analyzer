c5379597e4fdac0d84fd46f0bd01a5f786eb4197
anahitH <anahithayrapetyan91@gmail.com>
2017-10-29 16:34:43 +0100

    Updated tests and statistics files

---
+++ tests/bubble_sort/run-transformations.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+LIB_LOC=/usr/local/lib
+LOCAL_LIB_LOC=../../build/lib
+
+rm *.bc
+rm *.ll
+rm clone*
+rm extract*
+rm clone_on_extract*
+rm extract_on_clone*
+
+clang bubble_sort.cpp -c -emit-llvm
+
+
+#clone
+opt -load $LOCAL_LIB_LOC/libInputDependency.so -load $LOCAL_LIB_LOC/libTransforms.so bubble_sort.bc -clone-functions -o clone.bc
+
+#extract
+opt -load $LOCAL_LIB_LOC/libInputDependency.so -load $LOCAL_LIB_LOC/libTransforms.so bubble_sort.bc -extract-functions -o extract.bc
+
+#extract on clone
+opt -load $LOCAL_LIB_LOC/libInputDependency.so -load $LOCAL_LIB_LOC/libTransforms.so clone.bc -extract-functions -o extract_on_clone.bc
+
+#clone on extract
+#fails on input dep analysis
+#opt -load $LOCAL_LIB_LOC/libInputDependency.so -load $LOCAL_LIB_LOC/libTransforms.so extract.bc -clone-functions -o clone_on_extract.bc
+
+
+llvm-dis bubble_sort.bc
+llvm-dis clone.bc
+llvm-dis extract_on_clone.bc
+#llvm-dis clone_on_extract.bc
+
+clang bubble_sort.bc -lncurses -o bubble_sort
+clang clone.bc -lncurses -o clone
+clang extract.bc -lncurses -o extract
+clang extract_on_clone.bc -lncurses -o extract_on_clone
+#clang clone_on_extract.bc -lncurses -o clone_on_extract
+
